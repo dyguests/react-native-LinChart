1ab285198ddfc31148dfccfff7ad884d253029f5
fanhl <dyguests@qq.com>
2016-07-11 17:13:51 +0800

    init commit

--- index.ios.js
+++ index.ios.js
@@ -11,21 +11,46 @@ import {
     Text,
     View
 } from 'react-native';
+import HorizontalBarChart from './src/component/lin_charts/HorizontalBarChart';
 
 class LinChart extends Component {
+    // 构造
+    constructor(props) {
+        super(props);
+
+        let data = {
+            title: {
+                text: '世界人口总量',
+                subtext: '数据来自网络'
+            },
+            //主轴(在水平柱状图中相当于y轴,在柱状图中相当于x轴)
+            mAxis: {
+                type: 'category',
+                data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口']
+            },
+            series: [
+                {
+                    name: '2011年',
+                    type: 'bar',
+                    data: [182, 23489, 29034, 104970, 131744, 63230]
+                },
+                {
+                    name: '2012年',
+                    type: 'bar',
+                    data: [19325, 23438, 31000, 121594, 134141, 68107]
+                }
+            ]
+        };
+        // 初始状态
+        this.state = {
+            data: data,
+        };
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.ios.js
-                </Text>
-                <Text style={styles.instructions}>
-                    Press Cmd+R to reload,{'\n'}
-                    Cmd+D or shake for dev menu
-                </Text>
+                <HorizontalBarChart data={this.state.data}/>
             </View>
         );
     }
